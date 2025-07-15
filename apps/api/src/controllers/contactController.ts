371eb971156f08aa8a84290ed47422daa73f56c7
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-07-16 02:35:52 +0630

    update instagram username

--- apps/api/src/controllers/contactController.ts
+++ apps/api/src/controllers/contactController.ts
@@ -1,26 +1,26 @@
 import { Request, Response } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
-import { Redis } from "@upstash/redis";
+// import { Redis } from "@upstash/redis";
 
 dotenv.config();
 
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
-const redis = new Redis({
-  url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
-});
+// const redis = new Redis({
+//   url: process.env.REDIS_URL,
+//   token: process.env.REDIS_TOKEN,
+// });
 
 export const contactController = {
   store: async (req: Request, res: Response) => {
     try {
       const { name, email, message } = req.body;
 
-      await redis.lpush(
-        "contact_messages",
-        JSON.stringify({ name, email, message, date: new Date() }),
-      );
+      // await redis.lpush(
+      //   "contact_messages",
+      //   JSON.stringify({ name, email, message, date: new Date() }),
+      // );
 
       await axios.post(DISCORD_WEBHOOK_URL!, {
         content: `**New Contact Message**\n**Name**: ${name}\n**Email**: ${email}\n**Message**: ${message}`,
